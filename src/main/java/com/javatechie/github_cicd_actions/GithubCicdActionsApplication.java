7789a94fbeac1c9d8f0b6fc7f1615041292d7b96
Ramakant Shukla <Ramakantshukla190@gmail.com>
2025-11-09 17:21:29 +0530

    code

---
+++ src/main/java/com/javatechie/github_cicd_actions/GithubCicdActionsApplication.java
@@ -0,0 +1,21 @@
+package com.javatechie.github_cicd_actions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubCicdActionsApplication {
+
+//	echo "# github-actions-examples" >> README.md
+//	git init
+//	git add README.md
+//	git commit -m "first commit"
+//	git branch -M main
+//	git remote add origin https://github.com/RamakantShukla9118/github-actions-examples.git
+//	git push -u origin main
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubCicdActionsApplication.class, args);
+	}
+
+}
