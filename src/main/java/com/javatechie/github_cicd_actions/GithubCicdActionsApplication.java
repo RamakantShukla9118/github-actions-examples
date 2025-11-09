7e03fb4f29b16e049c7ad1051fce73bb12eb2da4
Ramakant Shukla <Ramakantshukla190@gmail.com>
2025-11-09 17:30:11 +0530

    Add endpoint

--- src/main/java/com/javatechie/github_cicd_actions/GithubCicdActionsApplication.java
+++ src/main/java/com/javatechie/github_cicd_actions/GithubCicdActionsApplication.java
@@ -2,17 +2,16 @@ package com.javatechie.github_cicd_actions;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class GithubCicdActionsApplication {
-
-//	echo "# github-actions-examples" >> README.md
-//	git init
-//	git add README.md
-//	git commit -m "first commit"
-//	git branch -M main
-//	git remote add origin https://github.com/RamakantShukla9118/github-actions-examples.git
-//	git push -u origin main
+	@GetMapping("/welcome")
+ public String welcome () {
+	 return "Welcome to JavaTechnie !";
+ }
 
 	public static void main(String[] args) {
 		SpringApplication.run(GithubCicdActionsApplication.class, args);
